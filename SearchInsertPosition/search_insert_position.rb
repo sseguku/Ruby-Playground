8a6b878ecf11c9eaacb72a99b3d514a04f54f537
Jjingo Kisakye <sseguku@gmail.com>
2022-09-13 01:56:27 +0300

    Search Insert Position Solution updated

--- SearchInsertPosition/search_insert_position.rb
+++ SearchInsertPosition/search_insert_position.rb
@@ -2,15 +2,32 @@
 
 # searches for insert position in an array
 class SearchInsertPositon
-  def position(_num, target)
-    target
+  def position(nums, target)
+    # position = num.index(target)
+    # position.nil? # target not available in array
+    return 0 if target < nums.first
+    return nums.size if target > nums.last
+
+    first = 0
+    last = nums.size - 1
+    while first <= last
+      mid = (first + ((last - first) / 2))
+      return mid if nums[mid] == target
+
+      if nums[mid] < target
+        first = mid + 1
+      else
+        last = mid - 1
+      end
+    end
+    first
   end
 end
 
 describe SearchInsertPositon do
   before(:each) do
     @search_position = SearchInsertPositon.new
-    @nums = [1, 3, 4, 6]
+    @nums = [1, 3, 5, 6]
   end
 
   it "nums = [1,3,5,6] target = 5 should output 2" do
@@ -20,6 +37,6 @@ describe SearchInsertPositon do
     expect(@search_position.position(@nums, 2)).to  eq(1)
   end
   it "nums = [1,3,5,6] target = 7 should output 4" do
-    expect(@search_position.position(@snums, 7)).to eq(4)
+    expect(@search_position.position(@nums, 7)).to eq(4)
   end
 end
